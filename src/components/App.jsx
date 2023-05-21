c950e8beb9307c55483630f21181571e1861cf8a
Nataliia-Shevchenko <nata_yuna@ukr.net>
2023-05-21 13:54:49 +0300

    update App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { fetchPictures } from '../services/pixabay-api';
 import { ToastContainer } from 'react-toastify';
 import { toast } from 'react-toastify';
@@ -8,21 +8,21 @@ import ImageGallery from './ImageGallery';
 import Loader from 'components/Loader';
 import Button from 'components/Button';
 
-class App extends Component {
-  state = {
-    searchText: '',
-    images: null,
-    loading: false,
-    error: '',
-    page: 1,
-    totalPages: 1,
-  };
+const App = () => {
+  const [searchText, setSearchText] = useState('');
+  const [images, setImages] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState('');
+  const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
 
-  async componentDidUpdate(prevProps, prevState) {
-    const { searchText, page } = this.state;
-    if (prevState.searchText !== searchText) {
+  useEffect(() => {
+    if (searchText === '') {
+      return;
+    }
+    async function fetch() {
       try {
-        this.setState({ loading: true });
+        setLoading(true);
         const { hits, totalHits } = await fetchPictures(searchText, page);
         if (hits.length === 0) {
           toast.error(
@@ -34,66 +34,47 @@ class App extends Component {
             return { id, largeImageURL, webformatURL, tags };
           }
         );
-
-        return this.setState({
-          images: filtredHits,
-          totalPages: Math.ceil(totalHits / 12),
-        });
-      } catch (error) {
-        this.setState({ error: error.message });
-        console.log(error);
+        if (page === 1) {
+          setImages(filtredHits);
+          setTotalPages(Math.ceil(totalHits / 12));
+        }
+        if (page > 1) {
+          setImages(prev => [...prev, ...filtredHits]);
+          setError('');
+        }
+      } catch (err) {
+        setError(err.message);
+        console.log(err);
       } finally {
-        this.setState({ loading: false });
+        setLoading(false);
       }
     }
 
-    if (prevState.page !== page) {
-      try {
-        this.setState({ loading: true });
-        const { hits } = await fetchPictures(searchText, page);
-        const filtredHits = hits.map(
-          ({ id, largeImageURL, webformatURL, tags }) => {
-            return { id, largeImageURL, webformatURL, tags };
-          }
-        );
-        return this.setState(prevState => ({
-          images: [...prevState.images, ...filtredHits],
-          error: '',
-        }));
-      } catch (error) {
-        this.setState({ error: error.message });
-        console.log(error);
-      } finally {
-        this.setState({ loading: false });
-      }
-    }
-  }
+    fetch();
+  }, [page, searchText]);
 
-  onSubmit = searchText => {
-    this.setState({ searchText, page: 1 });
+  const onSubmit = searchText => {
+    setSearchText(searchText);
+    setPage(1);
+    setImages(null);
   };
 
-  handleLoadMoreClick = () => {
-    this.setState(prevState => ({
-      page: prevState.page + 1,
-    }));
+  const handleLoadMoreClick = () => {
+    setPage(prevPage => prevPage + 1);
   };
 
-  render() {
-    const { images, error, loading, page, totalPages } = this.state;
-    return (
-      <>
-        <Searchbar onSubmit={this.onSubmit} />
-        {error && toast.error(error)}
-        {loading && <Loader />}
-        {images?.length > 0 && <ImageGallery images={images} />}
-        {images?.length >= 12 && page !== totalPages && (
-          <Button onClick={this.handleLoadMoreClick} />
-        )}
-        <ToastContainer autoClose={2000} />
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <Searchbar onSubmit={onSubmit} />
+      {error && toast.error(error)}
+      {loading && <Loader />}
+      {images?.length > 0 && <ImageGallery images={images} />}
+      {images?.length >= 12 && page !== totalPages && (
+        <Button onClick={handleLoadMoreClick} />
+      )}
+      <ToastContainer autoClose={2000} />
+    </>
+  );
+};
 
 export default App;
